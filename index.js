cc7131f8bec113eb1553658e304972b4c29eb412
nut077 <nuttapon.tavee@gmail.com>
2020-08-20 18:04:17 +0700

    complete

--- index.js
+++ index.js
@@ -11,28 +11,50 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 // Basic type ********************************
 var isDone = false;
+console.log(isDone); // false
 var decimal = 6;
+console.log(decimal); // 6
 var color = 'blue';
+console.log(color); // blue
 var list = [1, 2, 3];
+console.log(list); // [1, 2, 3]
 var list2 = [1, 2, 3];
+console.log(list2); // [1, 2, 3]
 var x;
-x = ["hello", 55];
-var notSure = 5;
-notSure = "meybe a string instead";
+x = ['hello', 55];
+console.log(x); //[ 'hello', 55 ]
+var notSure;
 notSure = false;
+console.log(notSure); // false
 function warnUser() {
-    console.log('This is my warning message');
+    console.log('This is my warning message void');
 }
+warnUser(); // This is my warning message void
 var u = undefined;
+console.log(u); // undefined
 var n = null;
-function error(message) {
-    throw new Error(message);
-}
+console.log(n); // null
+/*function error(message: string): never { // ไม่มีการ return ที่แท้จริงออกจาก function
+  throw new Error(message);
+}*/
 // Literal types ********************************
 var trafficLight;
-trafficLight = "Grean";
+trafficLight = 'Grean';
 // Enum ********************************
 var Color;
 (function (Color) {
@@ -40,7 +62,12 @@ var Color;
     Color[Color["GREEN"] = 1] = "GREEN";
     Color[Color["BLUE"] = 2] = "BLUE";
 })(Color || (Color = {}));
-var c = Color.GREEN;
+var r = Color.RED;
+var g = Color.GREEN;
+var b = Color.BLUE;
+console.log(r); // 0
+console.log(g); // 1
+console.log(b); // 2
 // Type Assertion ********************************
 var someValue = 'This is a string';
 var strLength = someValue.length;
@@ -126,30 +153,40 @@ function slugify(text) {
 console.log(slugify('Freedom Seven')); // freedom-seven
 console.log(slugify(['Hello', 'World'])); // hello-world
 function area(shape) {
-    return shape.kind === 'square' ?
-        Math.pow(shape.size, 2) : shape.width * shape.height;
+    return shape.kind === 'square'
+        ? Math.pow(shape.size, 2)
+        : shape.width * shape.height;
 }
 var areaSquare = {
     kind: 'square',
     size: 20
 };
 console.log(area(areaSquare)); // 400
-var areaRegtangle2 = {
+var areaRectangle2 = {
     kind: 'rectangle',
     width: 2,
     height: 4
 };
-console.log(area(areaRegtangle2)); // 8
+console.log(area(areaRectangle2)); // 8
 var point1 = { x: 2, y: 5 };
 var point2 = { x: 2, y: 5, z: 8 };
+console.log(point1); // { x: 2, y: 5 }
+console.log(point2); // {x: 2, y: 5, z: 8}
 // Parameter properties ********************************
 var ParameterProperties = /** @class */ (function () {
     function ParameterProperties(name, readonlyCanChangeInConstructorOnly) {
         this.name = name;
         this.readonlyCanChangeInConstructorOnly = readonlyCanChangeInConstructorOnly;
+        console.log(name);
     }
     return ParameterProperties;
 }());
+var p = new ParameterProperties('ParameterProperties', 'Readonly ParameterProperties');
+console.log(p); // ParameterProperties
+// ParameterProperties {
+//   name: 'ParameterProperties',
+//   readonlyCanChangeInConstructorOnly: 'Readonly ParameterProperties'
+// }
 // Accessors ********************************
 var Student = /** @class */ (function () {
     function Student(fullname) {
@@ -185,6 +222,7 @@ var Student = /** @class */ (function () {
 var freedomStudent = new Student('Freedom Seven');
 console.log(freedomStudent.firstName); // Freedom
 console.log(freedomStudent.lastName); // Seven
+console.log(freedomStudent.fullname); // Freedom Seven
 // Inheritance ********************************
 var Human = /** @class */ (function () {
     function Human(name) {
@@ -201,10 +239,213 @@ var Woman = /** @class */ (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Woman.prototype.pregnant = function () {
-        console.log('Pregnant');
+        console.log(this.name + " is pregnant");
     };
     return Woman;
 }(Human));
 var somsriWoman = new Woman('Somsri');
 somsriWoman.printName(); // Somsri
 somsriWoman.pregnant(); // Pregnant
+// Static properties ********************************
+var StaticCircle = /** @class */ (function () {
+    function StaticCircle(radius) {
+        this.radius = radius;
+    }
+    Object.defineProperty(StaticCircle.prototype, "area", {
+        get: function () {
+            return StaticCircle.PI * Math.pow(this.radius, 2);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    StaticCircle.PI = Math.PI;
+    return StaticCircle;
+}());
+var c1 = new StaticCircle(5);
+var c2 = new StaticCircle(10);
+console.log(c1.area); // 78.53
+console.log(c2.area); // 314.15
+// Abstract classes ********************************
+var AbstractShape = /** @class */ (function () {
+    function AbstractShape(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    return AbstractShape;
+}());
+var Rectangle4 = /** @class */ (function (_super) {
+    __extends(Rectangle4, _super);
+    function Rectangle4() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Rectangle4.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle4;
+}(AbstractShape));
+var Triangle = /** @class */ (function (_super) {
+    __extends(Triangle, _super);
+    function Triangle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Triangle.prototype.getArea = function () {
+        return (this.width * this.height) / 2;
+    };
+    return Triangle;
+}(AbstractShape));
+var abstractRectangle = new Rectangle4(2, 4);
+var abstractTriangle = new Triangle(2, 4);
+console.log(abstractRectangle.getArea()); // 8
+console.log(abstractTriangle.getArea()); // 4
+// Generic type ********************************
+var Stack = /** @class */ (function () {
+    function Stack() {
+        var _this = this;
+        this.data = [];
+        this.push = function (item) { return _this.data.push(item); };
+        this.prop = function () { return _this.data.pop(); };
+    }
+    Stack.prototype.print = function () {
+        console.log(this.data);
+    };
+    return Stack;
+}());
+// -------------------
+var stack = new Stack();
+stack.push('a');
+stack.push('b');
+stack.push('c');
+stack.prop();
+stack.print(); // [ 'a', 'b' ]
+function reverse(items, result) {
+    if (result === void 0) { result = []; }
+    var firstItem = items[0], restItems = items.slice(1);
+    console.log("firstItem = " + firstItem);
+    console.log("restItems = " + restItems);
+    console.log("result = " + result);
+    return items.length <= 0
+        ? result
+        : reverse(restItems, __spreadArrays([firstItem], result));
+}
+console.log(reverse([1, 2, 3, 4])); // [ 4, 3, 2, 1 ]
+// -------------------
+var Utility = /** @class */ (function () {
+    function Utility() {
+    }
+    Utility.reverse = function (items, result) {
+        if (result === void 0) { result = []; }
+        var firstItem = items[0], restItems = items.slice(1);
+        return items.length <= 0
+            ? result
+            : Utility.reverse(restItems, __spreadArrays([firstItem], result));
+    };
+    return Utility;
+}());
+console.log(Utility.reverse([1, 2, 3, 4])); // [ 4, 3, 2, 1 ]
+// Data descriptor ********************************
+var circleData = {};
+Object.defineProperty(circleData, 'PI', {
+    value: 3.14
+});
+console.log(circleData.PI); // 3.14
+// Accessor descriptor ********************************
+var circleAccessor = { _radius: 10 };
+Object.defineProperty(circleAccessor, 'radius', {
+    get: function () {
+        return this._radius;
+    },
+    set: function (radius) {
+        if (radius >= 0) {
+            this._radius = radius;
+        }
+    }
+});
+circleAccessor.radius = 9;
+console.log(circleAccessor.radius); // 9
+// Class decorators ********************************
+function logMethods(constructor) {
+    console.log(Object.keys(constructor.prototype));
+}
+var God = /** @class */ (function () {
+    function God() {
+    }
+    God.prototype.createTheWorld = function () {
+        console.log('Create the world');
+    };
+    ;
+    God.prototype.nap = function () {
+        console.log('Nap');
+    };
+    ;
+    God = __decorate([
+        logMethods
+    ], God);
+    return God;
+}());
+var god = new God(); // [ 'createTheWorld', 'nap' ]
+god.createTheWorld(); //Create the world
+god.nap(); // Nap
+// -------------------
+function logMethods2(startWidth) {
+    return function (construct) {
+        console.log(Object.keys(construct.prototype)
+            .filter(function (method) { return method.startsWith(startWidth); }));
+    };
+}
+var God2 = /** @class */ (function () {
+    function God2() {
+    }
+    God2.prototype.createTheWorld = function () {
+        console.log('God2 create the world');
+    };
+    God2.prototype.createHumans = function () {
+        console.log('God2 create humans');
+    };
+    God2.prototype.nap = function () {
+        console.log('God2 nap');
+    };
+    God2 = __decorate([
+        logMethods2('create')
+    ], God2);
+    return God2;
+}());
+var god2 = new God2(); // [ 'createTheWorld', 'createHumans' ]
+god2.createTheWorld(); // God2 create the world
+god2.createHumans(); // God2 create humans
+god2.nap(); // God2 nap
+// Property decorators ********************************
+function validate(regex) {
+    var _this = this;
+    return function (target, key) {
+        var _value = _this[key];
+        Object.defineProperty(target, key, {
+            set: function (value) {
+                if (regex.test(value)) {
+                    _value = value;
+                }
+                else {
+                    throw new Error('Invalid Value');
+                }
+            },
+            get: function () {
+                return _value;
+            }
+        });
+    };
+}
+var PersonPropertyDecorators = /** @class */ (function () {
+    function PersonPropertyDecorators() {
+    }
+    __decorate([
+        validate(/[a-zA-Z]+/)
+    ], PersonPropertyDecorators.prototype, "firstName");
+    __decorate([
+        validate(/[a-zA-Z]+/)
+    ], PersonPropertyDecorators.prototype, "lastName");
+    return PersonPropertyDecorators;
+}());
+var personP = new PersonPropertyDecorators();
+personP.firstName = 'aa';
+console.log(personP.firstName); // aa
+// Use for third party ********************************
+// https://github.com/DefinitelyTyped/DefinitelyTyped
--- index.ts
+++ index.ts
@@ -1,38 +1,53 @@
 // Basic type ********************************
 let isDone: boolean = false;
+console.log(isDone); // false
 let decimal: number = 6;
+console.log(decimal); // 6
 let color: string = 'blue';
+console.log(color); // blue
 let list: number[] = [1, 2, 3];
+console.log(list); // [1, 2, 3]
 let list2: Array<number> = [1, 2, 3];
+console.log(list2); // [1, 2, 3]
 let x: [string, number];
-x = ["hello", 55];
-let notSure: any = 5;
-notSure = "meybe a string instead";
+x = ['hello', 55];
+console.log(x); //[ 'hello', 55 ]
+let notSure: any;
 notSure = false;
+console.log(notSure); // false
 
 function warnUser(): void {
-  console.log('This is my warning message')
+  console.log('This is my warning message void');
 }
 
-let u: undefined = undefined
+warnUser(); // This is my warning message void
+
+let u: undefined = undefined;
+console.log(u); // undefined
 let n: null = null;
+console.log(n); // null
 
-function error(message: string): never { // ไม่มีการ return ที่แท้จริงออกจาก function
+/*function error(message: string): never { // ไม่มีการ return ที่แท้จริงออกจาก function
   throw new Error(message);
-}
+}*/
 
 // Literal types ********************************
 let trafficLight: 'Red' | 'Grean' | 'Yellow';
-trafficLight = "Grean";
+trafficLight = 'Grean';
 
 // Enum ********************************
 enum Color {
   RED,
   GREEN,
-  BLUE
+  BLUE,
 }
 
-let c: Color = Color.GREEN
+let r: Color = Color.RED;
+let g: Color = Color.GREEN;
+let b: Color = Color.BLUE;
+console.log(r); // 0
+console.log(g); // 1
+console.log(b); // 2
 
 // Type Assertion ********************************
 let someValue: any = 'This is a string';
@@ -41,8 +56,8 @@ console.log(strLength); // 16
 
 // Interface ********************************
 interface Rectangle {
-  width: number,
-  height: number
+  width: number;
+  height: number;
 }
 
 function getArea(rec: Rectangle): number {
@@ -52,14 +67,14 @@ function getArea(rec: Rectangle): number {
 console.log(getArea({width: 3, height: 4})); // 12
 
 interface Point2D {
-  x: number,
-  y: number
+  x: number;
+  y: number;
 }
 
 interface Point3D {
-  x: number,
-  y: number,
-  z: number
+  x: number;
+  y: number;
+  z: number;
 }
 
 function find2DDistance(point1: Point2D, point2: Point2D): number {
@@ -77,24 +92,24 @@ console.log(find2DDistance(point13D, point23D)); // 2.82
 
 // Optional properties ********************************
 interface State {
-  showPopup?: boolean,
-  showNotification?: boolean,
-  showMessage?: boolean
+  showPopup?: boolean;
+  showNotification?: boolean;
+  showMessage?: boolean;
 }
 
 const UI = {
   state: <State>{
     showPopup: false,
     showNotification: false,
-    showMessage: false
+    showMessage: false,
   },
   setState(state: State) {
-    Object.assign(this.state, state);
+    (<any>Object).assign(this.state, state);
   },
   printState() {
     console.log(this.state);
-  }
-}
+  },
+};
 UI.setState({showMessage: true});
 UI.printState(); //{ showPopup: false, showNotification: false, showMessage: true }
 
@@ -106,7 +121,7 @@ function display(person: { name: string }) {
 const somchai = {name: 'Somchai'};
 const somsri = {name: 'Somsri', age: 28};
 display(somchai); // Somchai
-display(somsri);  // Somsri
+display(somsri); // Somsri
 
 display({name: 'Somchai type fressness'});
 //display({name: 'Somsri type fressness', age: 28}) ไม่สามารถใส่ age เข้าไปได้ ต้องประกาศเป็นตัวแปร object ก่อนถึงจะสามารถส่งไปได้
@@ -138,19 +153,18 @@ console.log(circleClass.getArea()); // 314.159
 
 // Union type ********************************
 interface Shape2 {
-  name: string,
+  name: string;
 
-  getArea(): number
+  getArea(): number;
 }
 
 interface Logger2 {
-  name: string,
+  name: string;
 
-  log(): void
+  log(): void;
 }
 
 class Circle2 implements Shape2, Logger2 {
-
   constructor(private radius: number) {
   }
 
@@ -180,60 +194,77 @@ function slugify(text: string | string[]): string {
   return result.toLowerCase().replace(/ /g, '-');
 }
 
-console.log(slugify('Freedom Seven'));  // freedom-seven
+console.log(slugify('Freedom Seven')); // freedom-seven
 console.log(slugify(['Hello', 'World'])); // hello-world
 
 // Discriminated unions ********************************
 interface Square {
-  kind: 'square',
-  size: number
+  kind: 'square';
+  size: number;
 }
 
 interface Rectangle2 {
-  kind: 'rectangle',
-  width: number,
-  height: number
+  kind: 'rectangle';
+  width: number;
+  height: number;
 }
 
 type Shape3 = Square | Rectangle2;
 
 function area(shape: Shape3): number {
-  return shape.kind === 'square' ?
-    Math.pow(shape.size, 2) : shape.width * shape.height
+  return shape.kind === 'square'
+    ? Math.pow(shape.size, 2)
+    : shape.width * shape.height;
 }
 
 const areaSquare: Shape3 = {
   kind: 'square',
-  size: 20
+  size: 20,
 };
-console.log(area(areaSquare)) // 400
-const areaRegtangle2: Shape3 = {
+console.log(area(areaSquare)); // 400
+const areaRectangle2: Shape3 = {
   kind: 'rectangle',
   width: 2,
-  height: 4
+  height: 4,
 };
-console.log(area(areaRegtangle2)); // 8
+console.log(area(areaRectangle2)); // 8
 
 // Index signatures ********************************
 interface Point {
-  [key: string]: number
+  [key: string]: number;
 }
 
 const point1: Point = {x: 2, y: 5};
 const point2: Point = {x: 2, y: 5, z: 8};
-
+console.log(point1); // { x: 2, y: 5 }
+console.log(point2); // {x: 2, y: 5, z: 8}
 
 // Parameter properties ********************************
 class ParameterProperties {
   // public
+  // protected
   // private
   // readonly
   readonly readonlyCanChangeInConstructorOnly: string;
 
-  constructor(readonly name: string, readonlyCanChangeInConstructorOnly: string) {
+  constructor(
+    readonly name: string,
+    readonlyCanChangeInConstructorOnly: string
+  ) {
     this.readonlyCanChangeInConstructorOnly = readonlyCanChangeInConstructorOnly;
+    console.log(name);
   }
 }
+
+const p = new ParameterProperties(
+  'ParameterProperties',
+  'Readonly ParameterProperties'
+);
+console.log(p); // ParameterProperties
+// ParameterProperties {
+//   name: 'ParameterProperties',
+//   readonlyCanChangeInConstructorOnly: 'Readonly ParameterProperties'
+// }
 
 // Accessors ********************************
 class Student {
@@ -260,26 +291,224 @@ class Student {
     return `${this.firstName} ${this.lastName}`;
   }
 }
-const freedomStudent = new Student('Freedom Seven')
+
+const freedomStudent = new Student('Freedom Seven');
 console.log(freedomStudent.firstName); // Freedom
 console.log(freedomStudent.lastName); // Seven
-
+console.log(freedomStudent.fullname); // Freedom Seven
 
 // Inheritance ********************************
 class Human {
-  constructor(private name: string) {
+  constructor(protected name: string) {
   }
 
   printName() {
     console.log(this.name);
   }
 }
+
 class Woman extends Human {
   pregnant() {
-    console.log('Pregnant')
+    console.log(`${this.name} is pregnant`);
   }
 }
+
 const somsriWoman = new Woman('Somsri');
 somsriWoman.printName(); // Somsri
 somsriWoman.pregnant(); // Pregnant
+
+// Static properties ********************************
+class StaticCircle {
+  static PI = Math.PI;
+
+  constructor(private radius: number) {
+  }
+
+  get area(): number {
+    return StaticCircle.PI * Math.pow(this.radius, 2);
+  }
+}
+
+const c1 = new StaticCircle(5);
+const c2 = new StaticCircle(10);
+console.log(c1.area); // 78.53
+console.log(c2.area); // 314.15
+
+// Abstract classes ********************************
+abstract class AbstractShape {
+  constructor(protected width: number, protected height: number) {
+  }
+
+  public abstract getArea(): number;
+}
+
+class Rectangle4 extends AbstractShape {
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+class Triangle extends AbstractShape {
+  getArea(): number {
+    return (this.width * this.height) / 2;
+  }
+}
+
+const abstractRectangle = new Rectangle4(2, 4);
+const abstractTriangle = new Triangle(2, 4);
+console.log(abstractRectangle.getArea()); // 8
+console.log(abstractTriangle.getArea()); // 4
+
+// Generic type ********************************
+class Stack<T> {
+  private data: T[] = [];
+
+  push = (item: T): number => this.data.push(item);
+  prop = (): T => this.data.pop();
+
+  print(): void {
+    console.log(this.data);
+  }
+}
+
+// -------------------
+const stack: Stack<string> = new Stack<string>();
+stack.push('a');
+stack.push('b');
+stack.push('c');
+stack.prop();
+stack.print(); // [ 'a', 'b' ]
+
+function reverse<T>(items: T[], result: T[] = []): T[] {
+  const [firstItem, ...restItems] = items;
+  console.log(`firstItem = ${firstItem}`);
+  console.log(`restItems = ${restItems}`);
+  console.log(`result = ${result}`);
+  return items.length <= 0
+    ? result
+    : reverse(restItems, [firstItem, ...result]);
+}
+
+console.log(reverse([1, 2, 3, 4])); // [ 4, 3, 2, 1 ]
+
+// -------------------
+class Utility {
+  static reverse<T>(items: T[], result: T[] = []): T[] {
+    const [firstItem, ...restItems] = items;
+    return items.length <= 0
+      ? result
+      : Utility.reverse(restItems, [firstItem, ...result]);
+  }
+}
+
+console.log(Utility.reverse([1, 2, 3, 4])); // [ 4, 3, 2, 1 ]
+
+
+// Data descriptor ********************************
+const circleData: any = {};
+Object.defineProperty(circleData, 'PI', {
+  value: 3.14
+});
+console.log(circleData.PI); // 3.14
+
+
+// Accessor descriptor ********************************
+const circleAccessor: any = {_radius: 10};
+Object.defineProperty(circleAccessor, 'radius', {
+  get(): number {
+    return this._radius;
+  },
+  set(radius: any): void {
+    if (radius >= 0) {
+      this._radius = radius;
+    }
+  }
+});
+circleAccessor.radius = 9;
+console.log(circleAccessor.radius); // 9
+
+
+// Class decorators ********************************
+function logMethods(constructor: Function) {
+  console.log(Object.keys(constructor.prototype));
+}
+
+@logMethods
+class God {
+  createTheWorld() {
+    console.log('Create the world');
+  };
+
+  nap() {
+    console.log('Nap');
+  };
+}
+
+const god = new God(); // [ 'createTheWorld', 'nap' ]
+god.createTheWorld(); //Create the world
+god.nap(); // Nap
+// -------------------
+function logMethods2(startWidth: string) {
+  return (construct: Function) => {
+    console.log(
+      Object.keys(construct.prototype)
+        .filter(method => method.startsWith(startWidth)));
+  }
+}
+
+@logMethods2('create')
+class God2 {
+  createTheWorld() {
+    console.log('God2 create the world');
+  }
+
+  createHumans() {
+    console.log('God2 create humans');
+  }
+
+  nap() {
+    console.log('God2 nap');
+  }
+}
+const god2 = new God2(); // [ 'createTheWorld', 'createHumans' ]
+god2.createTheWorld(); // God2 create the world
+god2.createHumans(); // God2 create humans
+god2.nap(); // God2 nap
+
+
+// Property decorators ********************************
+function validate(regex: RegExp) {
+  return (target: any, key: string) => {
+    let _value: any = this[key];
+    Object.defineProperty(target, key, {
+      set(value: any): void {
+        if (regex.test(value)) {
+          _value = value;
+        } else {
+          throw new Error('Invalid Value');
+        }
+      },
+      get(): any {
+        return _value;
+      }
+    })
+  }
+}
+class PersonPropertyDecorators {
+  @validate(/[a-zA-Z]+/)
+  firstName: string;
+
+  @validate(/[a-zA-Z]+/)
+  lastName: string;
+}
+const personP = new PersonPropertyDecorators();
+personP.firstName = 'aa';
+console.log(personP.firstName); // aa
+
+
+// Use for third party ********************************
+// https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+
 
